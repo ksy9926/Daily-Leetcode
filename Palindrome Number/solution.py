a6e68deb9f1ff4036cacfa03a7b046c5e290f29e
수영 김 <ksy9926@naver.com>
2021-04-11 00:32:13 +0900

    Palindrome add solution2

--- Palindrome Number/solution.py
+++ Palindrome Number/solution.py
@@ -7,3 +7,17 @@ class Solution:
             if str_x[i] != str_x[-(i+1)]:
                 return False
         return True
+
+
+# Solution by Minjee
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x <0 : return False 
+        
+#         else: 
+#             if x == int(str(x)[::-1]): 
+                
+#                 return True 
+#             else:    
+#                 return False 
