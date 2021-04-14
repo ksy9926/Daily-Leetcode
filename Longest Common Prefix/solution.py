66700f24e9ee2dead5bff036067c7cafb56a8c01
수영 김 <ksy9926@naver.com>
2021-04-14 23:20:39 +0900

    Longest Common Prefix another solution

--- Longest Common Prefix/solution.py
+++ Longest Common Prefix/solution.py
@@ -14,3 +14,19 @@ class Solution:
             return result
         else:
             return ""
+
+# Solution by Minjee
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         x = ""
+#         if not strs :
+#             return x
+#         y = min(strs, key=len)
+#         for i in range(len(y)) :
+#             cur = strs[0][i]
+#             for j in range(1, len(strs)) :
+#                 if strs[j][i] != cur :
+#                     return x
+#             x = x + cur
+
+#         return ( x )
