749d8cb9fc3442e721ed814d5f86a31b53781d44
수영 김 <ksy9926@naver.com>
2021-04-14 23:21:44 +0900

    Roman to Integer another solution

--- Roman to Integer/solution.py
+++ Roman to Integer/solution.py
@@ -21,3 +21,17 @@ class Solution:
                 result += roman_dict[s[i]]
         
         return result
+
+
+# Solution by Minjee
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+   
+#         dic = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+#         x = len(s)
+#         result = dic[s[x-1]]
+#         for i in range(x-1,0,-1):
+#             cur = dic[s[i]]
+#             y = dic[s[i-1]]
+#             result += y if y >= cur else -y
+#         return result 
