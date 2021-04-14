64533c3d6a11609683c15e24f5eedcab0d7c94a2
수영 김 <ksy9926@naver.com>
2021-04-14 23:24:05 +0900

    Valid Parentheses another solution

--- Valid Parentheses/solution.py
+++ Valid Parentheses/solution.py
@@ -17,3 +17,25 @@ class Solution:
             return False
         return True
                 
+
+# Solution by Minjee
+
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         brackets={'}':'{',
+#                   ')':'(',
+#                   ']':'['} 
+           
+#         if len(s)<2:
+#             return False
+        
+#         for bracket in s:
+#             if bracket in brackets.values(): 
+#                 stack.append(bracket)
+#             else:
+#                 if not stack or brackets[bracket] != stack.pop():
+#                     return False
+#         if stack: 
+#             return False
+#         return True
